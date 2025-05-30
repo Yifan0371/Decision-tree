7b82cf2e498828ec9bea70e02567fa8e241d5fb2
Yifan <liyifan0117@gmail.com>
2025-05-30 14:11:19 +0200

    Revert "Optimizing the single decision tree of mse"
    This reverts commit 3dbec33202d6665c9756fa469528b5d0c2a53f4e.

--- include/criterion/MSECriterion.hpp
+++ include/criterion/MSECriterion.hpp
@@ -1,4 +1,3 @@
-// include/criterion/MSECriterion.hpp
 #ifndef MSE_CRITERION_HPP
 #define MSE_CRITERION_HPP
 
@@ -8,25 +7,6 @@ class MSECriterion : public ISplitCriterion {
 public:
     double nodeMetric(const std::vector<double>& labels,
                       const std::vector<int>& indices) const override;
-
-    // 新增：增量式计算接口，避免重复计算
-    struct MetricCache {
-        double sum = 0.0;
-        double sumSq = 0.0;
-        size_t count = 0;
-        double mean = 0.0;
-        double mse = 0.0;
-        bool valid = false;
-    };
-
-    // 计算指定索引范围的统计信息
-    static void calculateStats(const std::vector<double>& labels,
-                              const std::vector<int>& indices,
-                              MetricCache& cache);
-
-    // 增量式分割计算
-    static double splitMetric(const MetricCache& leftCache,
-                             const MetricCache& rightCache);
 };
 
 #endif // MSE_CRITERION_HPP
--- include/finder/ExhaustiveSplitFinder.hpp
+++ include/finder/ExhaustiveSplitFinder.hpp
@@ -1,9 +1,7 @@
-// include/finder/ExhaustiveSplitFinder.hpp
 #ifndef EXHAUSTIVE_SPLIT_FINDER_HPP
 #define EXHAUSTIVE_SPLIT_FINDER_HPP
 
 #include "../tree/ISplitFinder.hpp"
-#include "../criterion/MSECriterion.hpp"
 
 class ExhaustiveSplitFinder : public ISplitFinder {
 public:
@@ -14,33 +12,6 @@ public:
                   const std::vector<int>& indices,
                   double currentMetric,
                   const ISplitCriterion& criterion) const override;
-
-private:
-    // 高效的增量分割计算
-    struct SplitCandidate {
-        double value;
-        int index;
-        bool operator<(const SplitCandidate& other) const {
-            return value < other.value;
-        }
-    };
-
-    // 针对 MSE 的特化优化
-    std::tuple<int, double, double>
-    findBestSplitMSE(const std::vector<double>& data,
-                     int rowLength,
-                     const std::vector<double>& labels,
-                     const std::vector<int>& indices,
-                     double currentMetric) const;
-
-    // 通用分割查找
-    std::tuple<int, double, double>
-    findBestSplitGeneric(const std::vector<double>& data,
-                        int rowLength,
-                        const std::vector<double>& labels,
-                        const std::vector<int>& indices,
-                        double currentMetric,
-                        const ISplitCriterion& criterion) const;
 };
 
 #endif // EXHAUSTIVE_SPLIT_FINDER_HPP
